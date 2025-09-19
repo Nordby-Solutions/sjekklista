7392e48477c0463877fa4d9d8112ad48dbc09d20
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2025-09-19 19:38:48 +0200

    Landing: Better mail link

--- frontend/sjekklista-landing/app/layout.tsx
+++ frontend/sjekklista-landing/app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
           Dette produktet er under utvikling. For å melde interesse kan du ta
           kontakt{" "}
           <a
-            href="mailto:sebastianbjornstad@hotmail.com"
+            href="mailto:sebastianbjornstad@hotmail.com?subject=Hei,%20Sjekklista!%20Jeg%20er%20interessert%20i%20å%20prøve%20Sjekklista&body=Hei,%0D%0A%0D%0AJeg%20fant%20nettopp%20ut%20om%20Sjekklista%20og%20er%20interessert."
             className="underline font-medium text-yellow-800 hover:text-yellow-600"
           >
             her
