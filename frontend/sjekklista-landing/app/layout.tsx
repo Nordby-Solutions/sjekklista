eb23a164e07977ebcf03e761ec3e7fe06e9a0bf7
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2025-09-19 20:09:59 +0200

    Landing: Unami test

--- frontend/sjekklista-landing/app/layout.tsx
+++ frontend/sjekklista-landing/app/layout.tsx
@@ -93,13 +93,11 @@ export default function RootLayout({
         <TopNav />
         {children}
 
-        {process.env.NODE_ENV === "production" && (
-          <script
-            defer
-            src="https://cloud.umami.is/script.js"
-            data-website-id="92753677-5903-43ed-b05e-c06ae1a04b6e"
-          ></script>
-        )}
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="92753677-5903-43ed-b05e-c06ae1a04b6e"
+        ></script>
       </body>
     </html>
   );
