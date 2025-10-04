3a2502de6902cdfbb7bb46fa97e3dda855f38824
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2025-10-04 19:51:53 +0200

    Landing: Optimization

--- frontend/sjekklista-landing/components/LottiePlayer.tsx
+++ frontend/sjekklista-landing/components/LottiePlayer.tsx
@@ -10,12 +10,16 @@ export type LottiePlayerProps = {
 export default function LottiePlayer({ lottiePath, width }: LottiePlayerProps) {
   return (
     <div className="flex justify-center mb-8">
-      <Player
-        autoplay
-        loop
-        src={lottiePath}
-        className="sm:w-[250px] md:w-[300px] lg:w-[390px]"
-      />
+      <div className="w-[200px] h-[200px] sm:w-[250px] sm:h-[250px] md:w-[300px] md:h-[300px] lg:w-[390px] lg:h-[390px]">
+        <Player
+          autoplay
+          loop
+          src={lottiePath}
+          style={{ width: "100%", height: "100%" }}
+          renderer="canvas"
+          aria-hidden="true"
+        />
+      </div>
     </div>
   );
 }
