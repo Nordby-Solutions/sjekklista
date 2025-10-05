c3eed46104a5dbeb8822e909051887d7cd07d5b1
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2025-10-05 11:49:35 +0200

    Backend: Testing port

--- backend/sjekklista-backend/src/server.ts
+++ backend/sjekklista-backend/src/server.ts
@@ -7,6 +7,8 @@ import checklistRoutes from "./features/checklist-registration/checklist-routes"
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
@@ -15,6 +17,6 @@ app.use("/api", checklistRoutes);
 
 app.get("/health", (_, res) => res.send("I'm okay"));
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(port, () => {
+  console.log("Server running on http://localhost:" + port);
 });
