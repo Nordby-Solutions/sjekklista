b607ffcd3b11d542a04f7103a9813a8d007045b4
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2025-10-12 08:16:10 +0200

    changes

--- backend/sjekklista-backend/dist/features/checklist-registration/logic/createChecklist.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createChecklist = void 0;
-const createChecklist = async () => { };
-exports.createChecklist = createChecklist;
