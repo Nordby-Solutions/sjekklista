2b3f16eef0685f8104c3a7d190cfa83c5cf9db3d
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2025-10-15 20:28:33 +0200

    Test

--- backend/sjekklista-backend/src/lib/supabase.ts
+++ backend/sjekklista-backend/src/lib/supabase.ts
@@ -3,4 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
+console.error(supabaseUrl, supabaseKey);
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
