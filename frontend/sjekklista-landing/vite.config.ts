c967f5f37e80944e15425c91743674939525fa84
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2025-12-10 20:38:57 +0100

    Landing: Tailwind configuration (#22)

--- frontend/sjekklista-landing/vite.config.ts
+++ frontend/sjekklista-landing/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
