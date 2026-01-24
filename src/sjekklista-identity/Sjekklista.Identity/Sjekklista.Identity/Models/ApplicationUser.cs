94c9b2ad6a06595ba095969e2abb25a0a017bbb3
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2026-01-24 19:31:58 +0100

    Identity - Removed Sjekklista.Identity(Duende Identity server) because of not fitting community license (#35)

--- src/sjekklista-identity/Sjekklista.Identity/Sjekklista.Identity/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Sjekklista.Identity.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public Guid UserGuid { get; set; }
-    }
-}
