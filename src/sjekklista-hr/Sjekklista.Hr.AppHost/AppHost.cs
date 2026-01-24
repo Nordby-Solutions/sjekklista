32980a69db7f79092f3065af6098e32e35f34560
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2026-01-24 19:31:03 +0100

    Identity - Removed Sjekklista.Identity(Duende Identity server) because of not fitting community license

--- src/sjekklista-hr/Sjekklista.Hr.AppHost/AppHost.cs
+++ src/sjekklista-hr/Sjekklista.Hr.AppHost/AppHost.cs
@@ -1,13 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var identityService = builder.AddProject<Projects.Sjekklista_Identity>("identityservice")
-    .WithHttpHealthCheck("/health");
 
 var apiService = builder.AddProject<Projects.Sjekklista_Hr_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
 builder.AddJavaScriptApp("sjekklistahr", "../sjekklistahr")
-    //.WithHttpEndpoint(port: 5174, env: "PORT")
     .WithExternalHttpEndpoints()
     .WithEnvironment("VITE_API_BASE_URL", apiService.GetEndpoint("http"))
     .WithEnvironment("VITE_AUTH_AUTHORITY", "https://login.microsoftonline.com/5ea6f635-ad0f-4bd0-864b-05560252d210/v2.0/")
